c538eaec27729ece1ccb0706c7ae12e45f80c5b6
Javcon02061984 <jcg_0206@hotmail.com>
2021-11-11 16:14:45 -0500

    Initial commit

---
+++ src/test/java/mintic/LaGenerica/LaGenericaApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.LaGenerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaGenericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
