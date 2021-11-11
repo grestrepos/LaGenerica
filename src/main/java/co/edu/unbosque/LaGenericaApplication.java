0376885fe1ed7123415fdfc01205e65184f6eb02
grestrepos <gustavo.manduco@gmail.com>
2021-11-11 17:45:11 -0500

    actualizacion 1

--- src/main/java/co/edu/unbosque/LaGenericaApplication.java
+++ src/main/java/co/edu/unbosque/LaGenericaApplication.java
@@ -1,4 +1,4 @@
-package mintic.LaGenerica;
+package co.edu.unbosque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/co/edu/unbosque/microserviciosusuarios/model/Usuarios.java
@@ -0,0 +1,24 @@
+package co.edu.unbosque.microserviciosusuarios.model;
+
+import java.math.BigInteger;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Document(collection="db_usuarios")
+public class Usuarios {
+	
+	@Id
+	private BigInteger cedula_usuario;
+	private String nombre_usuario;
+	private String sede_usuario;
+	private String password_usuario;
+
+}
